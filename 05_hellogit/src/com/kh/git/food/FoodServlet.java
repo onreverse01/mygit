bc6429aa384182f263839b5a56cf1117204ef97e
onreverse01 <exploder211@naver.com>
2021-05-03 17:06:16 +0900

    menu 추가

--- 05_hellogit/src/com/kh/git/food/FoodServlet.java
+++ 05_hellogit/src/com/kh/git/food/FoodServlet.java
@@ -19,7 +19,7 @@ public class FoodServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("김밥 좋아요~").append("떡볶이 I love it!");
 	}
 
 }
